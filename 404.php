6ca7c3c62078a3eaf940dab6f757c2f880c84523
Lucas Sousa Alves <lucas.salves@terra.com.br>
2019-01-20 22:20:23 -0200

    Página 404

---
+++ 404.php
@@ -0,0 +1,18 @@
+
+
+ <?php get_header(); ?>
+ <div id="main">
+ 	<div id="separate-bar"></div>
+ 	<div id="content">
+ 		<h1 class="not-found">Página não encontrada! :( - 404</h1>
+ 		<a href="<?php echo get_home_url();  ?>">Página Inicial</a>
+ 	</div>
+ 	
+ 	<?php get_sidebar(); ?>
+
+ 	</div>
+ 	<div id="delimeter">
+ 		
+ 	</div>
+
+ 	<?php get_footer(); ?>
